b9b34db1952150659ce3225ee65dfec4116c071f
Arun Menon <avmenon@codeaurora.org>
2014-01-15 13:01:07 -0800

    msm: vidc: add control to enable Hierarchical P num layers
    This change will enable firmware to perform Hierarchical
    video encoding for P frames. Hierarchical video encoding is
    used to improve error resilience and temporal scalability.
    The encoded frames are logically organized in to multiple
    layers with frames in one layer referencing only frames
    from lower layers. The lowest layer, i.e. base layer, is
    the only exception.
    
    Change-Id: Ie30e6d075b76b3337ec895584cf1f3be9f4bb6c6
    Signed-off-by: Arun Menon <avmenon@codeaurora.org>
    
    Conflicts:
    	drivers/media/platform/msm/vidc/hfi_response_handler.c
    	drivers/media/platform/msm/vidc/msm_vidc_common.c
    	drivers/media/platform/msm/vidc/msm_vidc_internal.h
    	drivers/media/platform/msm/vidc/vidc_hfi_api.h

--- drivers/media/platform/msm/vidc/hfi_response_handler.c
+++ drivers/media/platform/msm/vidc/hfi_response_handler.c
@@ -420,6 +420,14 @@ static inline void copy_cap_prop(
 	case HFI_CAPABILITY_BITRATE:
 		out = &sess_init_done->bitrate;
 		break;
+
+	case HFI_CAPABILITY_ENC_LTR_COUNT:
+		out = &sess_init_done->ltr_count;
+		break;
+
+	case HFI_CAPABILITY_HIER_P_NUM_ENH_LAYERS:
+		out = &sess_init_done->hier_p;
+		break;
 	}
 
 	if (in && out) {
